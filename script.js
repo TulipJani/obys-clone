b53ba044bffc73f8c596cd3720bf58c8bfa7e48e
TulipJani <tkjani20@gmail.com>
2024-02-16 18:54:32 +0530

    page1 completed

--- script.js
+++ script.js
@@ -43,19 +43,19 @@ function loadingAnimation() {
   tl.from("nav", {
     y: -100,
   });
-  tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero3 h3, #hero4 h1", {
-    y: 120,
-    stagger: 0.075,
-    ease: "expo",
-    duration: 0.5,
-    delay: 0.2,
+  tl.from("#hero1 h1 ,#hero2 h1 ,#hero3 h2, #hero3 h3,#hero4 h1", {
+    y: 140,
+    stagger: 0.1,
+    duration: 0.4,
+    delay: -0.5,
   });
 }
 
 loadingAnimation();
 
-Shery.mouseFollower();
-Shery.makeMagnet(".magnet", {
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 1,
+document.addEventListener("mousemove", function (dets) {
+  gsap.to("#crsr", {
+    x: dets.clientX,
+    y: dets.clientY,
+  });
 });
